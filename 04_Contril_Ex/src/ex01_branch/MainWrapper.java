30f748b0b136196fe54e06feb21d8329a430a693
smc4718 <smc4718@naver.com>
2023-07-14 10:41:42 +0900

    230714

--- 04_Contril_Ex/src/ex01_branch/MainWrapper.java
+++ 04_Contril_Ex/src/ex01_branch/MainWrapper.java
@@ -129,11 +129,11 @@ public class MainWrapper {
   
   public static void ex05() {
     
-  //월에 따른 계절
-    //3 ~ 5 : 봄
-    //6 ~ 8 : 여름
-    //9 ~ 11: 가을
-    //12 ~ 2: 겨울
+  //월에 따른 계절      월 % 12
+    //3 ~ 5 : 봄         3 ~ 5
+    //6 ~ 8 : 여름       6 ~ 8
+    //9 ~ 11: 가을       9 ~ 11
+    //12 ~ 2: 겨울       0 ~ 2
     int month = 7;
     String season;  // "봄", "여름", "가을", "겨울"
     
@@ -149,6 +149,32 @@ public class MainWrapper {
     System.out.println(month + "월은 " + season + "입니다.");
   }
   
+  public static void ex05_2() {
+    
+    (위와 같은 문제, 다른 방식)
+    //월에 따른 계절      월 % 12
+      //3 ~ 5 : 봄         3 ~ 5
+      //6 ~ 8 : 여름       6 ~ 8
+      //9 ~ 11: 가을       9 ~ 11
+      //12 ~ 2: 겨울       0 ~ 2
+      int month = 7;
+      int mod = month % 12; // 월 % 12
+      String season;  // "봄", "여름", "가을", "겨울"
+      
+      if(mod <= 2) {
+        season = "겨울";
+      } else if(month <= 5) {
+        season = "봄";
+      } else if(month <= 8) {
+        season = "여름";
+      } else {
+        season = "겨울";
+      }
+      System.out.println(month + "월은 " + season + "입니다.");
+    }
+     
+  
+  
   public static void ex06() {
     
   //월에 따른 분기
@@ -156,11 +182,31 @@ public class MainWrapper {
     //4 ~ 6 : 2분기
     //7 ~ 9 : 3분기
     //10 ~ 12 : 4분기
-    int m = 7;
+    int month = 7;
     
+    if(month <= 3) {
+      System.out.println("1분기");
+    } else if(month <= 6) {
+      System.out.println("2분기");
+    } else if(month <= 9) {
+      System.out.println("3분기");
+    } else if(month <= 12) {
+      System.out.println("4분기");
+    }
+    System.out.println(month);
+    }
     
-    
-    
+    public static void ex06_2() {
+      
+      //(위와 같은 문제, 다른 방법)
+      //월에 따른 분기           분기 계산
+        //1 ~ 3 : 1분기         (month -1) / 3 + 1
+        //4 ~ 6 : 2분기
+        //7 ~ 9 : 3분기
+        //10 ~ 12 : 4분기
+        int month = 11;
+        System.out.println((month -1) / 3 + 1 + "분기");
+       
   }
   
   public static void ex07() {
@@ -169,7 +215,43 @@ public class MainWrapper {
     int day = 13;  // 13일은 목요일
     int nDay = 10;  // 10일
     String weekname;  // "월", "화", "수", "목", "금", "토", "일"
+    switch((day += nDay) % 7) {   //day = day + nDay
+      case 0: weekname = "금요일"; break;
+      case 1: weekname = "토요일"; break;
+      case 2: weekname = "일요일"; break;
+      case 3: weekname = "월요일"; break;
+      case 4: weekname = "화요일"; break;
+      case 5: weekname = "수요일"; break;
+      default: weekname = "목요일";
+    }
+    System.out.println(day + "일은 " + weekname + "입니다.");
+    //스위치 없이 배열로 더 간단하게 할 수 있다.
+  }
+  
+  public static void ex08() {
+    //대소문자 변환 (구글링으로 아스키코드 검색 후 참고)  = 아스키코드 : 아메리칸 스탠다드 코드
+    char ch = 'p';  //임의의 대문자 또는 소문자    //아스키코드 A = 65 , a = 97  || A ~ Z = 65 ~ 90 , a ~ z = 97 ~ 122
+     //각 아스키코드에 해당하는 숫자만큼 빼거나 더하면 소문자,대문자 자유롭게 변경 가능
+    if(ch >= 'A' && ch <= 'Z') {     //숫자 대신 문자를 넣어줘도 쉽게 가능.
+      ch += 32;
+    } else if(ch >= 'a' && ch <= 'z') {
+      ch -=32;
+    }
+    System.out.println(ch);
+  }
+  
+  public static void ex08_2() {
     
+    //위와 같은 문제, 다른 방법
+    //대소문자 변환 (구글링으로 아스키코드 검색 후 참고)  = 아스키코드 : 아메리칸 스탠다드 코드
+    char ch = 'a';  //임의의 대문자 또는 소문자    //아스키코드 A = 65 , a = 97  || A ~ Z = 65 ~ 90 , a ~ z = 97 ~ 122
+     
+    if(ch >= 65 && ch <= 90) {     
+      ch += 32;
+    } else if(ch >= 97 && ch <= 122) {
+      ch -=32;
+    }
+    System.out.println(ch);
   }
   
   public static void main(String[] args) {
@@ -177,8 +259,11 @@ public class MainWrapper {
   //ex02();
   //ex03();
   //ex03_2();
-  ex04();
-   
+  //ex04();
+  //ex05();
+  //ex06_2();
+  //ex07();
+  //ex08();
+    ex08_2();
   }
-
 }
