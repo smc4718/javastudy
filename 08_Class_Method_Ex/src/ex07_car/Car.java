5e06d7a037cfe025a02345aa68a8020dbf2dab38
smc4718 <smc4718@naver.com>
2023-07-21 15:24:39 +0900

    230721(3)

--- 08_Class_Method_Ex/src/ex07_car/Car.java
+++ 08_Class_Method_Ex/src/ex07_car/Car.java
@@ -6,7 +6,10 @@ public class Car {
   private Driver driver;
   private int speed;       //속도
   private int fuel;        //연료
-  private boolean isEmpty; //기름 없다.
+  private final int MAX_SPEED = 100;
+  private final int MIN_SPEED = 0;
+  
+  // final int 고정(상수)값.
   
   // Constructor
 
@@ -24,9 +27,8 @@ public class Car {
     this.fuel = fuel;
   }
   
-  public void setEmpty(boolean isEmpty) {
-    this.isEmpty = isEmpty;
-  }
+
+  
   // 이름을 setIsEmpty 가 아닌, setEmpty 로 한다.
   
   // Getter
@@ -42,10 +44,43 @@ public class Car {
     return fuel;
   }
   
-  public boolean isEmpty() {
-    return isEmpty;
-  }
+  
+  
   // 이름에 is가 들어가면 isIsEmpty 라고 하지 않고, isEmpty 라고 한다.
   
   // Method
+  
+  public void engineStart() {
+    if(fuel == 0) {
+      System.out.println("시동이 걸리지 않았습니다.");
+    } else {
+      System.out.println("시동이 걸렸습니다.");
+    }
+  }
+  
+  public void drive() {
+    if(fuel == 0 || driver == null)
+      //driver는 참조타입이기 때문에 null값을 가짐(운전자가 없다면 null상태)
+      System.out.println("자동차가 움직이지 않았습니다.");
+      else {
+      System.out.println("자동차가 움직입니다.");
+    }
+  }
+  
+  public void accel(int speed) {
+    this.speed += speed;
+    fuel--;
+    if(this.speed > MAX_SPEED) {
+      this.speed = MAX_SPEED;
+    }     
+    System.out.println("현재 속도는 " + this.speed + "입니다.");
+  }
+  
+  public void brake(int speed) {
+    this.speed -= speed;
+    if(this.speed < MIN_SPEED) {
+      this.speed = MIN_SPEED;
+    }
+    System.out.println("현재 속도는 " + this.speed + "입니다.");
+  }
 }
--- 08_Class_Method_Ex/src/ex07_car/Driver.java
+++ 08_Class_Method_Ex/src/ex07_car/Driver.java
@@ -1,5 +1,7 @@
 package ex07_car;
 
+import javax.management.monitor.MonitorSettingException;
+
 public class Driver {
 
   // Field
@@ -16,12 +18,15 @@ public class Driver {
     this.name = name;
   }
   
+  //경력에 따라 bestDriver 인지 결정하는 구문.
   public void setCareer(int career) {
     this.career = career;
+    setBestDriver(career >= 10);
+   // career 인수가 setBestDriver로 전달되고, 그 값에 따라 아래 boolean값에 들어가서 true,false 값이 나옴.
   }
   
-  public void setBestDrinver(boolean bestDriver) {
-    this.bestDriver = bestDriver;
+  public void setBestDriver(boolean bestDriver) {
+    this.bestDriver = bestDriver;  // 기준 career가 10년 이상이면 true
   }
   
   // Getter
@@ -33,7 +38,7 @@ public class Driver {
     return career;
   }
   
-  public boolean isBestDriver() {
+  private boolean isBestDriver() {
     return bestDriver;
   }
   // ★ boolean 타입은 getter의 이름규칙이 다르다.
