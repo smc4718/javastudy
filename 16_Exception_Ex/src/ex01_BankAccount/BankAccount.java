bdb617d892e4dbc49c64b11b4ac737611ee7d626
smc4718 <smc4718@naver.com>
2023-08-01 14:40:40 +0900

    230801(3)

--- 16_Exception_Ex/src/ex01_BankAccount/BankAccount.java
+++ 16_Exception_Ex/src/ex01_BankAccount/BankAccount.java
@@ -28,16 +28,54 @@ public class BankAccount {
    * @param money 입금할 금액
    * @exception RuntimeException 입금할 금액이 0보다 작거나 같은 경우에 발생한다.
    */
-  public void deposit(long money) {
-    try {
+  public void deposit(long money) throws RuntimeException {
     if(money <= 0) {
      throw new RuntimeException(money + "원 입금 불가");
     }
     balance += money;
-    } catch(RuntimeException e) { // 대신 Exception e 를 써도 됨.
+    }  // 대신 Exception e 를 써도 됨.
       System.out.println(e.getMessage());
-    }
+    
   }
-}
+  
+  /**
+   * 출금 메소드<br>
+   * 파라미터가 0보다 작거나 같으면 출금된 금액이 없으므로 0을 반환한다.
+   * 파라미터가 통장잔액(balance)보다 크면 출금된 금액이 없으므로 0을 반환한다.
+   * @param 출금할 금액
+   * @return 실제로 출금된 금액
+   * @exception RuntimeException 출금할 금액이 0 이하이거나 잔액보다 큰 경우에 발생한다.
+   */
+  public long withdrawal(long money) {
+    long retVal = 0;
+    if(money <= 0) {
+      throw new RuntimeException(money + "원 출금 불가");
+    } else if(money > balance)  {
+      throw new RuntimeException("잔액 부족");
+    } else {
+    balance -= money;
+      retVal = money;
+    }
+  } 
+    System.out.println(e.getMessage());
+  return retVal;
+  }    
+ 
 
-  // try - catch 가 없으면, 내가 예외 처리를 안하고 나를 호출하는 곳에서 예외 처리 하도록 전달하겠다.
+// try - catch 가 없으면, 내가 예외 처리를 안하고 나를 호출하는 곳에서 예외 처리 하도록 전달하겠다.
+
+/**
+ * 이체[ 메소드<br>
+ * 다른 계좌로 이체하는 메소드로 기존의 입금 메소드와 출금 메소드를 활용한다.<br>
+ * 내 계좌에서 출금을 먼저 수행하고, 출금된 금액만큼 다른 계좌로 입금한다.
+ * @param acc   다른 계좌
+ * @param money 이체할 금액
+ * @throws RuntimeException 입금 메소드와 출금 메소드가 던진 예외를 다시 던진다.
+ */
+  public void transfer(BankAccount acc, long money) throws RuntimeException {
+     acc.deposit(withdrawal(money));
+   }
+}
+  
+
+ 
--- 16_Exception_Ex/src/ex01_BankAccount/MainWrapper.java
+++ 16_Exception_Ex/src/ex01_BankAccount/MainWrapper.java
@@ -4,9 +4,17 @@ public class MainWrapper {
 
   public static void main(String[] args) {
     
-    BankAccount acc = new BankAccount(10000, "1234");
-    acc.deposit(-1);
-
+    try {
+      BankAccount acc = new BankAccount(10000, "1234");
+      BankAccount acc2 = new BankAccount(10000,"56789");
+      acc.deposit(10000);
+      acc2.withdrawal(5000);
+      acc.transfer(acc2, 1);
+      acc.inquiry();
+      acc2.inquiry();
+    } catch (RuntimeException e) {
+      System.out.println(e.getMessage());
+    }   //(★중요) BankAccount 에서 try-catch 를 다 제거하고 Main에서 하나로 모았다.
   }
 
 }
