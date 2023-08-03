51ccbaa2d9ffb2db7d84507e6a10e73d9e425a3f
smc4718 <smc4718@naver.com>
2023-08-03 16:52:51 +0900

    230803(5)

--- 18_Input_Output_Ex/src/ex02_delete/MainWrapper.java
+++ 18_Input_Output_Ex/src/ex02_delete/MainWrapper.java
@@ -3,16 +3,29 @@ package ex02_delete;
 import java.io.File;
 
 public class MainWrapper {
-  
-  public static void ex01() {
     
-    // C:/2023 디렉터리를 삭제하시오.
-    // 비어 있는 디렉터리만 삭제할 수 있다. 
+    /*
+    public static void ex01() {
+
+      // C:/2023 디렉터리를 삭제하시오.
+      // 비어 있는 디렉터리만 삭제할 수 있다.
+      
+      new File("C:/2023/08/03/15").delete();
+      new File("C:/2023/08/03").delete();
+      new File("C:/2023/08").delete();
+      new File("C:/2023").delete();
+      
+    }
+    */
     
-    new File("C:/2023/08/03/15").delete();
-    new File("C:/2023/08/03").delete();
-    new File("C:/2023/08").delete();
-    new File("C:/2023").delete();    
+    public static void ex01(File dir) {
+      File parent = dir.getParentFile();  // dir의 상위 디렉터리
+      if(parent == null) {                // dir의 상위 디렉터리가 없으면 그만
+        return;
+      }
+      System.out.println(dir.getPath() + " 디렉터리 삭제");
+      dir.delete();  // 현재 디렉터리 삭제
+      ex01(parent);  // 상위 디렉터리 전달
     
   }
 
