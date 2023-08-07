9587ce79055ddf570fba88f0b2aa4b8fe13a2f58
smc4718 <smc4718@naver.com>
2023-08-07 14:51:28 +0900

    230807(3)

---
+++ 20_Network_Ex/src/ex02_json_file/MainWrapper.java
@@ -0,0 +1,32 @@
+package ex02_json_file;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+   
+    // 문제. 아래 내용을 가지는 C:/storage/product.json 파일 생성하기
+    /*
+       [
+         {
+           "code" : "0001",
+           "name" : "냉장고",
+           "price": 100,
+           "sales : [
+             "하이마트",
+             "전자랜드"
+           ]
+         },
+         {
+           "code" : "0002",
+           "name  : "에어컨",
+           "price": 200,
+           "sales": [
+             "하이마트"
+           ]
+         }
+       ] 
+     */
+
+  }
+
+}
