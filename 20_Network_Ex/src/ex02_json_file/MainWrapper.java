f827c0f8e46ce244e0498b1d53eefe54d9834af9
smc4718 <smc4718@naver.com>
2023-08-07 15:41:08 +0900

    230807(4)

--- 20_Network_Ex/src/ex02_json_file/MainWrapper.java
+++ 20_Network_Ex/src/ex02_json_file/MainWrapper.java
@@ -1,9 +1,21 @@
 package ex02_json_file;
 
-public class MainWrapper {
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-  public static void main(String[] args) {
-   
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainWrapper {
+  
+  public static void ex01() {
+    
     // 문제. 아래 내용을 가지는 C:/storage/product.json 파일 생성하기
     /*
        [
@@ -26,7 +38,50 @@ public class MainWrapper {
          }
        ] 
      */
+     
+     // Map
+    Map<String, Object> map1 = new HashMap<String, Object>();
+    map1.put("code", "0001");
+    map1.put("name", "냉장고");
+    map1.put("price", 100);
+    map1.put("sales", Arrays.asList("하이마트", "전자랜드"));
+    
+    Map<String, Object> map2 = new HashMap<String, Object>();
+    map2.put("code", "0002");
+    map2.put("name", "에어컨");
+    map2.put("price", 100);
+    map2.put("sales", Arrays.asList("하이마트"));
 
+     // List
+    List<Map<String, Object>> list = Arrays.asList(map1, map2);
+    
+    // JSONArray
+    JSONArray array = new JSONArray(list);
+    
+    // JSONArray -> String
+    String products = array.toString();
+    
+    // 디렉터리 File 객체
+    File dir = new File("C:/storage");
+    if(dir.exists() == false) {
+      dir.mkdirs();
+    }
+    
+    //  파일 File 객체
+    File file = new File(dir, "product.json");
+    
+    // try - catch - resources (close를 생략할 수 있는 try 문)
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+      bw.write(products); 
+    System.out.println(file.getPath() + " 파일 생성 완료");
+    } catch (IOException e) {
+      e.printStackTrace();  
+      }
+    
   }
 
-}
+  public static void main(String[] args) {
+   
+    
+    
+    }
